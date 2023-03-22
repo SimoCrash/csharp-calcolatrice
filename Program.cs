d9594ca5d49c8c237e9a2bc06a74d7b5d5ac02fe
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-22 16:39:56 +0100

    Somma: numeri interi + numeri decimali

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using csharp_calcolatrice;
 
-Console.WriteLine($"Somma di due numeri interi: {IntNum()} + {IntNum()} = {CalcoliHelper.Somma(IntNum(), IntNum())}");
 Console.WriteLine($"Somma di due numeri interi: 2 + 3 = {CalcoliHelper.Somma(2, 3)}");
+Console.WriteLine($"Somma di due numeri decimali: 2.5 + 3.7 = {CalcoliHelper.Somma(2.5, 3.7)}");
 
-static int IntNum()
-{
-    Random random = new();
-    return random.Next(1, 4);
-}
+
 
 
