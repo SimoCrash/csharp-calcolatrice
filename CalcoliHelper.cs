c6027853e272b0951a2788d3d1dad3d6c99f2abe
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-22 16:42:43 +0100

    Differenza num int

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -10,5 +10,6 @@ namespace csharp_calcolatrice
     {   
         public static int Somma(int a, int b) => a + b;
         public static double Somma(double a, double b) => a + b;
+        public static int Differenza(int a, int b) => a - b;
     }
 }
