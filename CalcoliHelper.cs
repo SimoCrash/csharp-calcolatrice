f53458f6603d1a715e455dc09a324c3ed7cf0d25
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-22 18:25:09 +0100

    Gestione possibili errori NumMin Int

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace csharp_calcolatrice
 {
     public static class CalcoliHelper
-    {   
+    {
         public static int Somma(int a, int b) => a + b;
         public static double Somma(double a, double b) => a + b;
         public static int Differenza(int a, int b) => a - b;
@@ -16,12 +16,25 @@ namespace csharp_calcolatrice
         public static double Moltiplicazione(double a, double b) => a * b;
         public static int ValoreAssoluto(int a) => a >= 0 ? a : -a;
         public static double ValoreAssoluto(double a) => a >= 0 ? a : -a;
-        public static int NumMin(int a, int b)/* => Math.Min(a, b);*/ => a > b ? b : a;
+        public static int NumMin(int a, int b)/* => Math.Min(a, b);*/ /*=> a > b ? b : a;*/
+        {
+            
+            if (a > b)
+            {
+                return b;
+            } 
+            else if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
         public static double NumMin(double a, in double b) => a > b ? b : a;
         public static int NumMax(int a, int b) => a > b ? a : b;
         public static double NumMax(double a, double b) => a < b ? b : a;
-//- Massimo tra due numeri interi
-//- Massimo tra due numeri double
 
     }
 }
